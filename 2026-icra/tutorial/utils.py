59a33c2d84e5ee426265c1eecae956d285a3943c
anthonyjclark <anthonyjclark@gmail.com>
2026-05-28 13:23:05 -0700

    Added asset tutorial

--- 2026-icra/tutorial/utils.py
+++ 2026-icra/tutorial/utils.py
@@ -1,9 +1,18 @@
 from pathlib import Path
 
+from dataclasses import dataclass
+from math import pi
+
 import newton
 import rich
-import sphinx  # noqa: F401 for generating the tutorial website
 import warp as wp
+
+# Hack for generating tutorial website
+# Only import sphinx if running on local machine (check hostname)
+import platform
+if platform.node().startswith("AJC"):
+    import sphinx  # noqa: F401 for generating the tutorial website
+
 
 # Path is hardcoded in Newton's viewer for generating docs
 RECORDING_BASE_PATH = Path("docs/_static/")
@@ -39,3 +48,47 @@ def create_viewer(filename: str, model: newton.Model, quiet: bool = True) -> new
         viewer.log_arrows(label, starts, ends, axis, width=0.04)
 
     return viewer
+
+
+@dataclass
+class FrankaEmikaPandaConfig:
+    FINGER_CLOSED = 0.0
+    FINGER_OPEN = 0.04
+
+    HOME_Q_ARM = [0.0, 0.02, 0.0, -2.37, 0.0, 2.39, pi / 4]
+    HOME_Q_GRIPPER = [FINGER_OPEN, FINGER_OPEN]
+    HOME_Q = HOME_Q_ARM + HOME_Q_GRIPPER
+
+    DOF_COUNT = len(HOME_Q)
+    ARM_DOF_COUNT = len(HOME_Q_ARM)
+
+    EFFORT_LIMITS = [87, 87, 87, 87, 12, 12, 12, 100, 100]
+    MUJOCO_ARMATURE = [0.195] * 4 + [0.074] * 3 + [0.1] * 2
+
+    TARGET_KE = [900, 900, 700, 700, 400, 400, 400, 100, 100]
+    TARGET_KD = [90, 90, 70, 70, 40, 40, 40, 10, 10]
+    BODY_GRAVCOMP = 1.0
+
+
+def enable_target_control(builder):
+    """Enable position targets and MuJoCo gravity compensation for Franka."""
+    builder.joint_target_pos[:_config.DOF_COUNT] = _config.HOME_Q
+    builder.joint_target_ke[:_config.DOF_COUNT] = _config.TARGET_KE
+    builder.joint_target_kd[:_config.DOF_COUNT] = _config.TARGET_KD
+
+    joint_actgravcomp = builder.custom_attributes["mujoco:jnt_actgravcomp"].values
+    for dof_index in range(_config.DOF_COUNT):
+        joint_actgravcomp[dof_index] = True
+
+    body_gravcomp = builder.custom_attributes["mujoco:gravcomp"].values
+    for body_index, label in enumerate(builder.body_label):
+        if label.startswith("fr3/") and label not in {"fr3/base", "fr3/fr3_link0"}:
+            body_gravcomp[body_index] = _config.BODY_GRAVCOMP
+
+def set_initial_state(builder):
+    """Set the initial joint positions and armature."""
+    builder.joint_q[:_config.DOF_COUNT] = _config.HOME_Q
+    builder.joint_effort_limit[:_config.DOF_COUNT] = _config.EFFORT_LIMITS
+    builder.joint_armature[:_config.DOF_COUNT] = _config.MUJOCO_ARMATURE
+
+_config = FrankaEmikaPandaConfig()
